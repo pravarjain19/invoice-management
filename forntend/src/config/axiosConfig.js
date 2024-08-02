12c1c868e4b99cbe03781e6d4777283d4301d989
pravar jain <103636656+pravarjain19@users.noreply.github.com>
2024-08-02 23:52:06 +0530

    Update axiosConfig.js

--- forntend/src/config/axiosConfig.js
+++ forntend/src/config/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
- baseURL: 'http://13.201.18.239:8081/v1/invoice/', // replace with your API base URL
+ baseURL: 'http://13.126.76.196:8081/v1/invoice/', // replace with your API base URL
   headers: {
     'Content-Type': 'application/json',
     // Add any other default headers here
